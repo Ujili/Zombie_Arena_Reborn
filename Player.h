9ee2ebe448d341bbbe25c036213478d56eebd395
Todd Crooks <tcrooks22@gmail.com>
2023-08-25 15:51:08 -0400

    v0.8.203

--- Player.h
+++ Player.h
@@ -45,6 +45,58 @@ private:
 	float m_Speed;
 
 	//Public functions to follow!
+public:
 
+	Player();
+
+	void spawn(IntRect arena, Vector2f resolution, int tileSize);
+
+	//Call this at the end of every game
+	void resetPlayerStats();
+
+	//Handle getting hit
+	bool hit(Time timeHit);
+
+	//How long ago was last hit
+	Time getLastHitTime();
+
+	//Where is the player
+	FloatRect getPosition();
+
+	//Where is the center of the player
+	Vector2f getCenter();
+
+	//What angle is the player facing
+	float getRotation();
+
+	//Send a copy of the sprite to the main function
+	Sprite getSprite();
+
+	//These move the player
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void moveDown();
+
+	//Stop the player from moving
+	void stopLeft();
+	void stopRight();
+	void stopUp();
+	void stopDown();
+
+	//Update function, once per frame
+	void update(float elapsedTime, Vector2i mousePosition);
+
+	//Get a speeeeed boooooost!
+	void upgradeSpeed();
+
+	//Healing
+	void upgradeHealth();
+
+	//Increase max health
+	void increaseHealthLevel(int amount);
+
+	//What is current health?
+	int getHealth();
 
 };
